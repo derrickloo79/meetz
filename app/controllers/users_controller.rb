246f5efb01fdfcd5bbc8281bf99df95b12083146
Derrick <derrickloo@hey.com>
2024-12-24 11:12:40 +0800

    Update visual for name, org and admin.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
     before_action :require_admin, only: [:destroy]
 
     def index
-        @users = User.all.where(admin: false).order("name")
+        @users = User.all.order("name")
     end
 
     def new
@@ -14,6 +14,7 @@ class UsersController < ApplicationController
     def create
         @user = User.new(user_params)
         if @user.save
+            session[:user_id] = @user.id
             redirect_to @user, notice: "Thanks for signing up!"
         else
             render :new, status: :unprocessable_entity
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,56 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Room.create!([
+    {
+        name: 'Main meeting room',
+        room_type: 'Meeting Room'
+    },
+    {
+        name: 'Privacy booth 1',
+        room_type: 'Phone Booth'
+    },
+    {
+        name: 'Privacy booth 2',
+        room_type: 'Phone Booth'
+    },
+    {
+        name: 'Privacy booth 3',
+        room_type: 'Phone Booth'
+    },
+])
+User.create!([
+    {
+        name: "Derrick Loo",
+        email: 'derrick.loo@omnihr.co',
+        organization: 'Omni HR',
+        admin: true,
+        password: 'secret',
+        password_confirmation: 'secret'
+    },
+    {
+        name: "Brian Ip",
+        email: 'brian.ip@omnihr.co',
+        organization: 'Omni HR',
+        admin: true,
+        password: 'secret',
+        password_confirmation: 'secret'
+    },
+    {
+        name: "Nara",
+        email: 'nara@ideaink.co',
+        organization: 'Idea Ink',
+        admin: true,
+        password: 'secret',
+        password_confirmation: 'secret'
+    },
+    {
+        name: "Yu Hu",
+        email: 'yuhu@kaito.ai',
+        organization: 'Kaito',
+        admin: true,
+        password: 'secret',
+        password_confirmation: 'secret'
+    },
+])
