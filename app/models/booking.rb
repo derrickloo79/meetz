ee409cb1b439f6af17d79c15531de372d33745f9
Derrick <derrickloo@hey.com>
2024-12-24 15:57:46 +0800

    Added logic to ensure endtime later than starttime.

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -3,6 +3,7 @@ class Booking < ApplicationRecord
   belongs_to :user
   
   validate :no_time_clash
+  validate :end_time_after_start_time
 
   validates :title, presence: true
 
@@ -13,6 +14,12 @@ private
                           .where.not(id: id)
     if overlapping_bookings.exists?
       errors.add(:base, "There is already a booking during this time.")
+    end
+  end
+
+  def end_time_after_start_time
+    if end_time.present? && start_time.present? && end_time <= start_time
+      errors.add(:end_time, "End time must be after the start time.")
     end
   end
 end
