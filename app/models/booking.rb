78d797ff115a4a4c581cd04b95fc99d0fc0084f8
Derrick <derrickloo@hey.com>
2024-12-24 10:03:20 +0800

    Added logic to avoid room booking timing clashes.

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,6 +1,18 @@
 class Booking < ApplicationRecord
   belongs_to :room
   belongs_to :user
+  
+  validate :no_time_clash
 
   validates :title, presence: true
+
+private
+
+  def no_time_clash
+    overlapping_bookings = Booking.where("room_id = ? AND ((start_time < ? AND end_time > ?))", room_id, end_time, start_time)
+                          .where.not(id: id)
+    if overlapping_bookings.exists?
+      errors.add(:base, "There is already a booking during this time.")
+    end
+  end
 end
